e0521eebe80def708d62861d1898fe4c5a06ecc0
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2022-03-28 15:00:45 -0400

    day one progress

--- script.js
+++ script.js
@@ -2,76 +2,94 @@ const chooseRandomStudentButton = document.getElementById("chooseRandomStudentIn
 const saveListButton = document.getElementById("saveListInput");
 const loadSavedListButton = document.getElementById("loadSavedListInput");
 const clearListButton = document.getElementById("clearListInput");
-const assignRandomTeamsButton = document.getElementById("assignRandomTeamsInput")
-const numberOfTeamsNumber = document.getElementById("numberOfTeamsInput")
+const assignRandomTeamsButton = document.getElementById("assignRandomTeamsInput");
+const numberOfTeamsNumber = document.getElementById("numberOfTeamsInput");
 const studentListTextarea = document.getElementById("studentListInput");
 const randomChoiceSpan = document.getElementById("randomChoice");
 
-const debug = true;
+const debug = false;
 
 var savedStudentList;
 
 // Event listeners are added for the interface in this block.
 
-chooseRandomStudentButton.addEventListener('click', chooseRandomStudent);
-assignRandomTeamsButton.addEventListener('click', assignRandomTeams)
-saveListButton.addEventListener('click', saveList);
-loadSavedListButton.addEventListener('click', loadList);
-clearListButton.addEventListener('click', clearList)
+chooseRandomStudentButton.addEventListener("click", chooseRandomStudent);
+assignRandomTeamsButton.addEventListener("click", assignRandomTeams);
+saveListButton.addEventListener("click", saveList);
+loadSavedListButton.addEventListener("click", loadList);
+clearListButton.addEventListener("click", clearList);
 
 function generateStudentList() {
-    let list = studentListTextarea.value.split(/\r\n|\r|\n/g);
-    if (debug == true) {
-        console.log(list);
-    }
-    return list;
+  let list = studentListTextarea.value.split(/\r\n|\r|\n/g);
+  if (debug == true) {
+    console.log(list);
+  }
+  return list;
 }
 
 function assignRandomTeams() {
-    let numberOfTeams = numberOfTeamsNumber.value;
-    let studentList = generateStudentList();
-    if (debug == true) {
-        console.log(numberOfTeams);
-        console.log(studentList);
+  let numberOfTeams = numberOfTeamsNumber.value;
+  let studentList = generateStudentList();
+  if (debug == true) {
+    console.log(numberOfTeams);
+    console.log(studentList);
+  }
+  // Need to choose a student at random and then assign them to teamN
+  // Then the program should choose another student and assign them to teamN + 1
+  // Finally when N is greater than numberOfTeams it should reset N to 1
+  let teams = [];
+  const teamSize = Math.floor(studentList.length / numberOfTeams);
+  const leftover = studentList.length % teamSize;
+  if (studentList.length >= numberOfTeams) {
+    for (let i = 0; i < numberOfTeams; i++) {
+      let team = [];
+      for (let j = 0; j < teamSize; j++) {
+        const index = Math.floor(Math.random() * (studentList.length - 1));
+        team.push(studentList[index]);
+        studentList.splice(index, 1);
+      }
+      teams.push(team);
     }
-    // Need to choose a student at random and then assign them to teamN
-    // Then the program should choose another student and assign them to teamN + 1
-    // Finally when N is greater than numberOfTeams it should reset N to 1
-    while (studentList != "") {
-        for (let i = 0; i <= numberOfTeams; i++) {
-            
-        }
+    let leftOverTeamNumber = 0;
+    while (leftover > 0) {
+      teams[leftOverTeamNumber].push(studentList[0]);
+      studentList.splice(0, 1);
+      leftOverTeamNumber++;
     }
+
+    console.log(teams);
+    return teams;
+  }
 }
 
 function chooseRandomStudent() {
-    let studentList = generateStudentList();
-    let randomInteger = generateRandomInteger(0, studentList.length - 1);
-    if (debug == true) {
-        console.log(studentList);
-        console.log(randomInteger);
-    }
-    let choice = studentList[randomInteger];
-    if (debug == true) {
-        console.log(choice)
-    }
-    studentList.splice(randomInteger, 1);
-    studentListTextarea.value = studentList.join("\n");
-    randomChoiceSpan.textContent = choice;
+  let studentList = generateStudentList();
+  let randomInteger = generateRandomInteger(0, studentList.length - 1);
+  if (debug == true) {
+    console.log(studentList);
+    console.log(randomInteger);
+  }
+  let choice = studentList[randomInteger];
+  if (debug == true) {
+    console.log(choice);
+  }
+  studentList.splice(randomInteger, 1);
+  studentListTextarea.value = studentList.join("\n");
+  randomChoiceSpan.textContent = choice;
 }
 
 function clearList() {
-    studentListTextarea.value = "";
+  studentListTextarea.value = "";
 }
 
 function generateRandomInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function loadList() {
-    studentListTextarea.value = savedStudentList;
+  studentListTextarea.value = savedStudentList;
 }
 
 function saveList() {
-    savedStudentList = studentListTextarea.value;
+  savedStudentList = studentListTextarea.value;
 }
