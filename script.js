d738ea34c4b5bf1eac3c016d7f6da5e76fc5bc68
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2022-11-15 10:34:49 -0500

    major refactoring

--- script.js
+++ script.js
@@ -12,103 +12,98 @@ const studentListTextarea = document.getElementById("studentListInput");
 const randomChoiceSpan = document.getElementById("randomChoice");
 const teamsParagraph = document.querySelector("#teamsParagraph");
 
-const debug = false;
-
-var savedStudentList;
-
-// Event listeners are added for the interface in this block.
+let savedStudentList;
 
 chooseRandomStudentButton.addEventListener("click", chooseRandomStudent);
-assignRandomTeamsButton.addEventListener("click", assignRandomTeams);
-saveListButton.addEventListener("click", saveList);
-loadSavedListButton.addEventListener("click", loadList);
-clearListButton.addEventListener("click", clearList);
-
-studentListTextarea.addEventListener("input", () => {
-	studentListTextarea.value = studentListTextarea.value.replace(",", "\n");
-});
-
-function generateStudentList() {
-	let list = studentListTextarea.value.split(/\r\n|\r|\n/g);
-	if (debug == true) {
-		console.log(list);
-	}
-	return list;
-}
-
-function assignRandomTeams() {
-	let numberOfTeams = numberOfTeamsNumber.value;
-	let studentList = generateStudentList();
-	if (debug == true) {
-		console.log(numberOfTeams);
-		console.log(studentList);
-	}
-	// Need to choose a student at random and then assign them to teamN
-	// Then the program should choose another student and assign them to teamN + 1
-	// Finally when N is greater than numberOfTeams it should reset N to 1
-	let teams = [];
-	const teamSize = Math.floor(studentList.length / numberOfTeams);
-	if (studentList.length >= numberOfTeams) {
-		for (let i = 0; i < numberOfTeams; i++) {
-			let team = [];
-			for (let j = 0; j < teamSize; j++) {
-				const index = Math.floor(Math.random() * (studentList.length - 1));
-				team.push(studentList[index]);
-				studentList.splice(index, 1);
-			}
-			teams.push(team);
-		}
-		let leftOverTeamNumber = 0;
-		let leftover = studentList.length;
-		while (leftover > 0) {
-			teams[leftOverTeamNumber].push(studentList[0]);
-			studentList.splice(0, 1);
-			leftover--;
-			leftOverTeamNumber++;
-		}
-		let outputString = "";
-		teams.forEach((team, teamNumber) => {
-			outputString += `Team ${teamNumber + 1}: <ul>`;
-			team.forEach((member) => {
-				outputString += `<li>${member} </li>`;
-			});
-			outputString += "</ul>";
-		});
-		teamsParagraph.innerHTML = outputString;
-	} else {
-		teamsParagraph.innerHTML =
-			"There are not enough students to make this many teams!";
-	}
-}
-
 function chooseRandomStudent() {
 	let studentList = generateStudentList();
 	let randomInteger = generateRandomInteger(0, studentList.length - 1);
-	if (debug == true) {
-		console.log(studentList);
-		console.log(randomInteger);
-	}
 	let choice = studentList[randomInteger];
-	if (debug == true) {
-		console.log(choice);
-	}
+
 	studentList.splice(randomInteger, 1);
 	studentListTextarea.value = studentList.join("\n");
 	randomChoiceSpan.textContent = choice;
 }
 
-function clearList() {
+assignRandomTeamsButton.addEventListener("click", assignRandomTeams);
+function assignRandomTeams() {
+	let numberOfTeams = numberOfTeamsNumber.value;
+	let studentList = generateStudentList();
+	let teams = [];
+	const teamSize = Math.floor(studentList.length / numberOfTeams);
+
+	if (!(studentList.length >= numberOfTeams)) {
+		teamsParagraph.innerText =
+			"There are not enough students to make this many teams!";
+		return;
+	}
+
+	// add as many students as possible to each team, until there are less students then teams left
+	for (let teamCount = 0; teamCount < numberOfTeams; teamCount++) {
+		let newTeam = [];
+		for (let studentCount = 0; studentCount < teamSize; studentCount++) {
+			const randomStudentIndex = Math.floor(
+				Math.random() * (studentList.length - 1)
+			);
+			newTeam.push(studentList[randomStudentIndex]);
+			studentList.splice(randomStudentIndex, 1); //remove the student from the list of remaining students
+		}
+		teams.push(newTeam);
+	}
+
+	let teamNumberForLeftover = 0; // the index of the team to add the leftover to
+	let leftoverStudents = studentList.length; // the amount of left over students
+
+	while (leftoverStudents > 0) {
+		teams[teamNumberForLeftover].push(studentList[0]); //add the leftover student to a team
+		studentList.splice(0, 1); //remove the student from the list of remaining students
+		leftoverStudents--;
+		teamNumberForLeftover++;
+	}
+
+	let output = document.createDocumentFragment();
+
+	teams.forEach((team, teamNumber) => {
+		const title = document.createElement("div");
+		const teamList = document.createElement("ul");
+		title.innerText = `Team ${teamNumber + 1}`;
+
+		output.appendChild(title);
+		output.appendChild(teamList);
+
+		team.forEach((teamMember) => {
+			const member = document.createElement("li");
+			member.innerHTML = teamMember;
+			teamList.appendChild(member);
+		});
+	});
+
+	teamsParagraph.replaceChildren(output);
+}
+
+saveListButton.addEventListener("click", () => {
+	savedStudentList = studentListTextarea.value;
+});
+
+loadSavedListButton.addEventListener("click", () => {
+	studentListTextarea.value = savedStudentList;
+});
+
+clearListButton.addEventListener("click", () => {
 	studentListTextarea.value = "";
+});
+
+studentListTextarea.addEventListener("input", () => {
+	studentListTextarea.value = studentListTextarea.value.replace(",", "\n");
+});
+
+// HELPER FUNCTIONS
+function generateStudentList() {
+	const text = studentListTextarea.value; //get the string value of the textarea content
+	const list = text.split(/\r\n|\r|\n/g); //split the string into an array of the values between new line characters
+	return list;
 }
 
 function generateRandomInteger(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function loadList() {
-	studentListTextarea.value = savedStudentList;
-}
-
-function saveList() {
-	savedStudentList = studentListTextarea.value;
 }
